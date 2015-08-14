6edcda82bace04857c6ec365e3dd2306dd06e51d
Nikita Took <654070@gmail.com>
2015-08-14 15:53:03 +0200

    added nav bar button to refresh

---
+++ Pizza/NTBarButtonItem.h
@@ -0,0 +1,17 @@
+//
+//  NTBarButtonItem.h
+//  Pizza
+//
+//  Created by Nikita Tuk on 14/08/15.
+//  Copyright (c) 2015 Nikita Took. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void (^NTBarButtonItemAction)(UIBarButtonItem *sender);
+
+@interface NTBarButtonItem : UIBarButtonItem
+
+- (instancetype)initWithImage:(UIImage *)image actionBlock:(NTBarButtonItemAction)actionBlock;
+
+@end
---
+++ Pizza/NTBarButtonItem.m
@@ -0,0 +1,49 @@
+//
+//  NTBarButtonItem.m
+//  Pizza
+//
+//  Created by Nikita Tuk on 14/08/15.
+//  Copyright (c) 2015 Nikita Took. All rights reserved.
+//
+
+#import "NTBarButtonItem.h"
+
+@interface NTBarButtonItem ()
+
+@property (nonatomic, strong) UIButton *button;
+@property (nonatomic, copy) NTBarButtonItemAction actionBlock;
+@end
+
+@implementation NTBarButtonItem
+
+- (instancetype)initWithImage:(UIImage *)image actionBlock:(NTBarButtonItemAction)actionBlock {
+    
+    self = [super init];
+    
+    if (self) {
+        
+        self.actionBlock = actionBlock;
+        _button = [UIButton buttonWithType:UIButtonTypeSystem];
+        self.customView = _button;
+        [_button setImage:image forState:UIControlStateNormal];
+        [_button addTarget:self action:@selector(buttonTouched) forControlEvents:UIControlEventTouchUpInside];
+        _button.frame = CGRectMake(0.0, 0.0, image.size.width, image.size.height);
+        _button.userInteractionEnabled = YES;
+        
+    }
+    
+    return self;
+    
+}
+
+- (void)buttonTouched {
+    
+    if (self.actionBlock) {
+        
+        self.actionBlock(self);
+        
+    }
+    
+}
+
+@end
