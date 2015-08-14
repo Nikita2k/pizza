ced7ee13c6a7ca181e4158db6267af93ebb6f99e
Nikita Tuk <tuknik@yandex-team.ru>
2015-08-14 14:12:34 +0200

    Initial Commit

---
+++ Pizza/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pizza
+//
+//  Created by Nikita Tuk on 14/08/15.
+//  Copyright (c) 2015 Nikita Took. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
